3745a51ae35bd452549b04e0322cfe322d0aa72c
Akshat Srivastava <chikusrivastava535@gmail.com>
2026-07-07 16:06:57 +0530

    fix: set CORS origin to true to resolve all CORS blocking issues on deploy

--- backend/app.js
+++ backend/app.js
@@ -17,23 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "http://localhost:5174",
-  "http://127.0.0.1:5174",
-  process.env.FRONTEND_URL,
-].filter(Boolean);
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
